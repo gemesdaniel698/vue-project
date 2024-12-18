70c9054849b74cd3f72aa3407344d71892ef01b6
istvan <istvan.marhefka@gmail.com>
2024-12-18 10:21:47 +0100

    make cards available in global variable

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,24 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App)
-.use(router)
-.mount('#app')
+let cards = [
+    { id: 1, title: 'German words - Lektion 4' },
+    { id: 2, title: 'German words - Lektion 3' },
+    { id: 3, title: 'German words - Lektion 2' },
+    { id: 4, title: 'German words - Lektion 1' },
+    { id: 5, title: 'German words - Lektion 4' },
+    { id: 6, title: 'German words - Lektion 3' },
+    { id: 7, title: 'German words - Lektion 2' },
+    { id: 8, title: 'German words - Lektion 1' },
+    { id: 9, title: 'English - Unit 7' },
+    { id: 10, title: 'Chemistry definitions' },
+  ];
+
+const app = createApp(App)
+app.config.globalProperties.$cards = cards;
+
+app.use(router);
+app.mount('#app')
 
 // main.js
 import 'bootstrap/dist/css/bootstrap.min.css';
--- src/views/menu.vue
+++ src/views/menu.vue
@@ -47,18 +47,7 @@ export default {
   data() {
     return {
       searchQuery: '',
-      cards: [
-        { id: 1, title: 'German words - Lektion 4' },
-        { id: 2, title: 'German words - Lektion 3' },
-        { id: 3, title: 'German words - Lektion 2' },
-        { id: 4, title: 'German words - Lektion 1' },
-        { id: 5, title: 'German words - Lektion 4' },
-        { id: 6, title: 'German words - Lektion 3' },
-        { id: 7, title: 'German words - Lektion 2' },
-        { id: 8, title: 'German words - Lektion 1' },
-        { id: 9, title: 'English - Unit 7' },
-        { id: 10, title: 'Chemistry definitions' },
-      ],
+      cards: this.$cards,
     };
   },
   computed: {
