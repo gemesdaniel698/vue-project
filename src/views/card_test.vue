8e869057efec625f03bb3ed3aa1bdc5fc035b576
agoston <agostonheier@gmail.com>
2024-12-21 22:53:12 +0100

    yes and no

--- src/views/card_test.vue
+++ src/views/card_test.vue
@@ -12,25 +12,35 @@
  <body>
   <div id="app ">
     <!-- Main Container -->
-    <div class="container px-4 ">
-      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-       <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="1000000">
-         <img src="/src/pictures/haus.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item" data-bs-interval="1000000">
-         <img src="/src/pictures/haz.jpg" class="d-block w-100" alt="...">
-        </div>
-       </div>
-       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-         <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-         <span class="visually-hidden">Next</span>
-        </button>
-</div>
+    <div class="container-xxl px-4 ">
+     <div class="row align-items-center">
+      <div class="col-md-2"> 
+       <button type="button" class="btn btn-success">Success</button>
+      </div>  
+      <div class="col-md-6"> 
+         <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+           <div class="carousel-item active" data-bs-interval="1000000">
+            <img src="/src/pictures/haus.jpg" class="d-block w-75" alt="...">
+           </div>
+           <div class="carousel-item" data-bs-interval="1000000">
+            <img src="/src/pictures/haz.jpg" class="d-block w-75" alt="...">
+           </div>
+          </div>
+           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+           </button>
+           <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+           </button>
+         </div>
+      </div>
+      <div class="col-md-2"> 
+         <button type="button" class="btn btn-danger">Danger</button>
+      </div>
+     </div>
       <!-- Fixed Button -->
       <button class="btn btn-info btn-fixed">Fixed Button</button>
     </div>
